fixed mixed precision

kigo/nn.py
@@ -23,6 +23,18 @@ class SinusoidalEmbedding(hk.Module):
         return jnp.concatenate([jnp.sin(xs), jnp.cos(xs)], axis=-1)
 
 
+def fp32(fn: Callable[[jnp.ndarray], jnp.ndarray]
+         ) -> Callable[[jnp.ndarray], jnp.ndarray]:
+
+    def inner(x: jnp.ndarray) -> jnp.ndarray:
+        return fn(x.astype(jnp.float32)).astype(x.dtype)
+
+    return inner
+
+
+silu = fp32(jax.nn.silu)
+
+
 class Attention(hk.Module):
 
     def __init__(self,
@@ -36,7 +48,7 @@ class Attention(hk.Module):
         self.scale = head_channels ** -0.5
         hidden_channels = head_channels * heads
         self.heads = heads
-        self.norm = hk.GroupNorm(groupnorm_groups)
+        self.norm = fp32(hk.GroupNorm(groupnorm_groups))
         self.to_qkv = hk.Conv2D(hidden_channels * 3, 1, with_bias=False)
         self.out_proj = hk.Conv2D(channels, 1,
                                   w_init=hk.initializers.Constant(0.),
@@ -45,7 +57,7 @@ class Attention(hk.Module):
     def __call__(self, xt: jnp.ndarray) -> jnp.ndarray:
         _, h, w, _ = xt.shape
         xt_norm = self.norm(xt)
-        xt_norm = jax.nn.silu(xt_norm)
+        xt_norm = silu(xt_norm)
         # Compute queries, keys and values
         qkv = jnp.split(self.to_qkv(xt_norm), 3, axis=-1)
         q, k, v = [rearrange(arr, "b x y (h c) -> b h c (x y)", h=self.heads)
@@ -55,7 +67,7 @@ class Attention(hk.Module):
         pattern = "b h d i, b h d j -> b h i j"
         sim: jnp.ndarray = jnp.einsum(pattern, q, k)
         sim = sim - jnp.amax(sim, axis=-1, keepdims=True)
-        attn: jnp.ndarray = jax.nn.softmax(sim, axis=-1)
+        attn: jnp.ndarray = fp32(jax.nn.softmax)(sim)
         # Compute the values for each head
         pattern = "b h i j, b h d j -> b h i d"
         out = jnp.einsum(pattern, attn, v)
@@ -76,9 +88,9 @@ class ResBlock(hk.Module):
                  ) -> None:
         super().__init__(name)
         self.dropout = dropout
-        self.norm_1 = hk.GroupNorm(groupnorm_groups)
+        self.norm_1 = fp32(hk.GroupNorm(groupnorm_groups))
         self.conv_1 = hk.Conv2D(channels, 3)
-        self.norm_2 = hk.GroupNorm(groupnorm_groups)
+        self.norm_2 = fp32(hk.GroupNorm(groupnorm_groups))
         self.affine = hk.Linear(channels * 2,
                                 w_init=hk.initializers.Constant(0.),
                                 b_init=hk.initializers.Constant(0.))
@@ -94,7 +106,7 @@ class ResBlock(hk.Module):
     def __call__(self, xt: jnp.ndarray, snr_emb: jnp.ndarray, training: bool
                  ) -> jnp.ndarray:
         h = self.norm_1(xt)
-        h = jax.nn.silu(h)
+        h = silu(h)
         h = self.conv_1(h)
         h = self.norm_2(h)
         wb = self.affine(snr_emb)
@@ -104,7 +116,7 @@ class ResBlock(hk.Module):
         h = h * (1. + w) + b
         if training:
             h = hk.dropout(hk.next_rng_key(), self.dropout, h)
-        h = jax.nn.silu(h)
+        h = silu(h)
         h = self.conv_2(h)
         h = h + self.residual_conv(xt)
         if self.attention is not None:
@@ -125,7 +137,7 @@ class UBlock(hk.Module):
         ch = self.ub_cfg.channels
         self.in_proj = hk.Sequential([
             hk.GroupNorm(self.ub_cfg.groupnorm_groups),
-            jax.nn.silu,
+            silu,
             (hk.Conv2D(ch, 1)
              if is_outer else
              hk.Conv2D(ch, 3, stride=2)),
@@ -156,7 +168,7 @@ class UBlock(hk.Module):
         ]
         self.out_proj = hk.Sequential([
             hk.GroupNorm(self.ub_cfg.groupnorm_groups),
-            jax.nn.silu,
+            silu,
             (hk.Conv2D(outer_channels, 1)  # type: ignore
              if is_outer else
              hk.Conv2DTranspose(outer_channels, 3, stride=2)),
@@ -195,16 +207,16 @@ class Model(hk.Module):
                              is_outer=True)
         self.out_proj = hk.Sequential([
             hk.GroupNorm(self.model_cfg.outer_groupnorm_groups),
-            jax.nn.silu,
+            silu,
             hk.Conv2D(self.model_cfg.output_channels, 3,
                       w_init=hk.initializers.Constant(0.),
                       b_init=hk.initializers.Constant(0.)),
         ])
         self.snr_mlp = hk.Sequential([
             SinusoidalEmbedding(self.model_cfg.snr_sinusoidal_embedding_width),
-            jax.nn.silu,
+            silu,
             hk.Linear(self.model_cfg.snr_embedding_width),
-            jax.nn.silu,
+            silu,
         ])
 
     @classmethod

kigo/training.py
@@ -17,8 +17,7 @@ from .diffusion import sample_q, sample_p, cosine_snr
 from .utils import Directory, get_logger, random_name
 from .configs import Config
 from .data import Dataset
-from .nn import (Model, SinusoidalEmbedding, Attention,
-                 get_params_and_forward_fn)
+from .nn import (Model, SinusoidalEmbedding, get_params_and_forward_fn)
 from . import persistence
 
 
@@ -28,6 +27,7 @@ logger = get_logger('kigo.training')
 @dataclass
 class Context:
     iteration: int = 0
+    loss_scale: int = 2 ** 15
     wandb_run_id: Optional[str] = None
 
     def periodically(self, freq: int, skip_first: bool = True) -> bool:
@@ -46,14 +46,22 @@ class Pack:
     opt_state: optax.OptState
     rngs: hk.PRNGSequence
     mae: float
+    scale: jmp.DynamicLossScale
 
 
 def get_opt_and_opt_state(cfg: Config,
                           params: optax.Params,
                           opt_state: Optional[optax.OptState] = None,
                           ) -> Tuple[optax.MultiSteps, optax.OptState]:
-    opt = optax.adamw(learning_rate=cfg.tr.learning_rate,
-                      weight_decay=cfg.tr.weight_decay)
+
+    def lr_schedule(step: jnp.ndarray) -> jnp.ndarray:
+        return jnp.minimum(1., step / cfg.tr.learning_rate_warmup_steps)
+
+    opt = optax.chain(
+        optax.adamw(learning_rate=cfg.tr.learning_rate,
+                    weight_decay=cfg.tr.weight_decay),
+        optax.scale_by_schedule(lr_schedule),
+    )
     ms_opt = optax.MultiSteps(opt, cfg.tr.gradient_accumulation_steps)
     opt_state = ms_opt.init(params) if opt_state is None else opt_state
     return ms_opt, opt_state
@@ -69,7 +77,8 @@ def train(params: optax.Params,
           ctx: Context,
           ) -> Iterator[Pack]:
 
-    scale = jmp.DynamicLossScale(jnp.asarray(2 ** 15))
+    scale = jmp.DynamicLossScale(jnp.asarray(ctx.loss_scale),
+                                 period=cfg.tr.dynamic_loss_period)
 
     def forward_fn(x0: jnp.ndarray,
                    snr: jnp.ndarray,
@@ -92,20 +101,7 @@ def train(params: optax.Params,
 
     gradient_fn = jax.grad(loss_fn, has_aux=True)
     opt, opt_state = get_opt_and_opt_state(cfg, params, opt_state)
-
-    # Set policies for mixed precision
-    half, full = jnp.float16 if cfg.tr.use_fp16 else jnp.float32, jnp.float32
-    hk.mixed_precision.set_policy(SinusoidalEmbedding,
-                                  jmp.Policy(param_dtype=full,
-                                             compute_dtype=full,
-                                             output_dtype=half))
-    hk.mixed_precision.set_policy(Attention, jmp.Policy(param_dtype=full,
-                                                        compute_dtype=full,
-                                                        output_dtype=half))
-    model_policy = jmp.Policy(param_dtype=full,
-                              compute_dtype=half,
-                              output_dtype=full)
-    hk.mixed_precision.set_policy(Model, model_policy)
+    model_policy = set_mixed_precision_policies(cfg.tr.use_fp16)
 
     @partial(jax.pmap, axis_name='device', donate_argnums=5)
     def train_step(x0: jnp.ndarray,
@@ -119,17 +115,19 @@ def train(params: optax.Params,
         rng, rng_split = jax.random.split(rng)
         noise = jax.random.normal(rng, shape=x0.shape)
         rng, rng_split = jax.random.split(rng)
-        t = jax.random.uniform(rng_split, shape=(len(x0),))
-        snr = cosine_snr(t)
+        snr = cosine_snr(jax.random.uniform(rng_split, shape=(len(x0),)))
         xt = sample_q(x0, noise, snr)
         gradients, (loss, scale_) = gradient_fn(params_, xt, snr, noise,
                                                 scale_, rng)
         gradients = model_policy.cast_to_compute(gradients)
-        gradients = scale.unscale(gradients)
+        gradients = scale_.unscale(gradients)
         gradients = jax.lax.pmean(gradients, axis_name='device')
         gradients = model_policy.cast_to_param(gradients)
+        gradients_finite = jmp.all_finite(gradients)
+        scale_ = scale_.adjust(gradients_finite)
         updates, opt_state_ = opt.update(gradients, opt_state_, params=params_)
-        params_ = optax.apply_updates(params_, updates)
+        new_params_ = optax.apply_updates(params_, updates)
+        params_ = jmp.select_tree(gradients_finite, new_params_, params_)
         ema_ = optax.incremental_update(params_, ema_,
                                         step_size=1. - cfg.tr.ema_alpha)
         return params_, ema_, opt_state_, scale_, loss ** 0.5
@@ -164,6 +162,8 @@ def train(params: optax.Params,
             p_params, p_ema, p_opt_state, p_scale, p_mae = state
             maes.append(float(p_mae.mean()))
         ctx.iteration += 1
+        scale = pytree_collapse(p_scale)
+        ctx.loss_scale = int(scale.loss_scale)
         if ctx.iteration % cfg.tr.yield_freq == 0:
             yield Pack(workdir=workdir,
                        cfg=cfg,
@@ -172,7 +172,21 @@ def train(params: optax.Params,
                        ema=pytree_collapse(p_ema),
                        opt_state=pytree_collapse(p_opt_state),
                        rngs=rngs,
-                       mae=jnp.mean(jnp.array(maes)))
+                       mae=jnp.mean(jnp.array(maes)),
+                       scale=scale)
+
+
+def set_mixed_precision_policies(use_fp16: bool) -> jmp.Policy:
+    half, full = jnp.float16 if use_fp16 else jnp.float32, jnp.float32
+    sin_emb_policy = jmp.Policy(param_dtype=full,
+                                compute_dtype=full,
+                                output_dtype=half)
+    model_policy = jmp.Policy(param_dtype=full,
+                              compute_dtype=half,
+                              output_dtype=full)
+    hk.mixed_precision.set_policy(SinusoidalEmbedding, sin_emb_policy)
+    hk.mixed_precision.set_policy(Model, model_policy)
+    return model_policy
 
 
 def autosave(packs: Iterator[Pack]) -> Iterator[Pack]:
@@ -249,7 +263,8 @@ def _get_wandb_run(pack: Pack) -> WandbRun:
 
 
 def _log_to_wandb(run: WandbRun, pack: Pack, mean_mae: float) -> None:
-    run.log({'MAE': mean_mae}, step=pack.ctx.iteration)
+    d = {'MAE': mean_mae, 'Loss-Scale': float(pack.scale.loss_scale)}
+    run.log(d, step=pack.ctx.iteration)
 
 
 def _img_to_wandb(run: WandbRun, pack: Pack) -> None: