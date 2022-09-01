added gradient clipping, fixed sampling

kigo/diffusion.py
@@ -58,13 +58,13 @@ def sample_p_step(xt: jnp.ndarray,
              * gt0((1 - snr_next) / gt0(1 - snr)) ** 0.5
              * gt0((1 - snr) / gt0(snr_next)) ** 0.5)
     # Eq. 9 in DDIM
-    x0_hat = (xt - gt0(1. - snr) ** 0.5 * noise_pred) / gt0(snr ** 0.5)
+    x0_hat = (xt - gt0(1. - snr) ** 0.5 * noise_pred) / gt0(snr) ** 0.5
     # Dynamic thresholding from Imagen by the Google Brain Team.
     s = jnp.quantile(jnp.abs(x0_hat), clip_percentile, axis=(1, 2, 3),
                      keepdims=True)
-    xt = jnp.where(s > 1.,
-                   jnp.clip(x0_hat, -s, s) / gt0(s),
-                   x0_hat)
+    x0_hat = jnp.where(s > 1.,
+                       jnp.clip(x0_hat, -s, s) / gt0(s),
+                       x0_hat)
     # Eq. 12 in DDIM
     xt = (x0_hat * gt0(snr_next) ** 0.5
           + noise_pred * gt0(1. - snr_next - sigma ** 2) ** 0.5